2b2ef66caed34077b408d5a35e3b38a8d1277f02
Fahdii Ajmalal Fikrie <fahdiaf@gmail.com>
2021-12-06 20:01:45 +0700

    feat: add page title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import 'styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Tugas Pemfung – FAAS</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
